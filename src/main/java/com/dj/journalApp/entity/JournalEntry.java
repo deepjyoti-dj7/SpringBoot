cb3e9488e85740c53724490727377fc9e08febcd
Deepjyoti <deepjyoti0072@gmail.com>
2025-06-19 00:36:13 +0530

    Added Lombok dependency

--- src/main/java/com/dj/journalApp/entity/JournalEntry.java
+++ src/main/java/com/dj/journalApp/entity/JournalEntry.java
@@ -1,13 +1,14 @@
 package com.dj.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -15,16 +16,4 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() { return date; }
-    public void setDate(LocalDateTime date) { this.date = date; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public ObjectId getId() { return id; }
-    public void setId(ObjectId id) { this.id = id; }
 }
