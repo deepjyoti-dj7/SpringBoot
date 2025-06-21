13b51133495c7834cf9e32cf15be420a248969dc
Deepjyoti <deepjyoti0072@gmail.com>
2025-06-22 02:01:06 +0530

    Added Transactions

--- src/main/java/com/dj/journalApp/JournalApplication.java
+++ src/main/java/com/dj/journalApp/JournalApplication.java
@@ -2,6 +2,10 @@ package com.dj.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -12,5 +16,8 @@ public class JournalApplication {
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
-
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
 }
