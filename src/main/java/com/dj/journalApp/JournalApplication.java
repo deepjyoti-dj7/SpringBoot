3e81a53d9ebeb1a95d51e855d177fd6634084e1f
Deepjyoti <deepjyoti0072@gmail.com>
2025-06-22 01:33:47 +0530

    Updated APIs

--- src/main/java/com/dj/journalApp/JournalApplication.java
+++ src/main/java/com/dj/journalApp/JournalApplication.java
@@ -2,12 +2,15 @@ package com.dj.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/dj/journalApp/service/JournalEntryService.java
+++ src/main/java/com/dj/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,16 +23,13 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
-            try {
-                User user = userService.findByUserName(userName);
-                journalEntry.setDate(LocalDateTime.now());
-                JournalEntry saved = journalEntryRepository.save(journalEntry);
-                user.getJournalEntries().add(saved);
-                userService.saveEntry(user);
-            } catch (Exception e) {
-                log.error("Exception" , e);
-            }
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
     }
 
     public void saveEntry(JournalEntry journalEntry) {
