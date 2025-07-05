623d056eff1d0a8a0df5b38fa898be58f8beb885
Deepjyoti <deepjyoti0072@gmail.com>
2025-07-05 21:23:51 +0530

    Added @Value

--- src/main/java/com/dj/journalApp/service/WeatherService.java
+++ src/main/java/com/dj/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.dj.journalApp.service;
 
 import com.dj.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    private static final String apiKey = "517e4ed871f9e897d233e06896ef94d3";
+    @Value("${weather.api.key}")
+    private String apiKey;
+
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
