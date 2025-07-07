798ea284510eb0a23bbaa3a356707af34348d9e7
Deepjyoti <deepjyoti0072@gmail.com>
2025-07-08 00:14:53 +0530

    Added SMTP mail sender

---
+++ src/test/java/com/dj/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,20 @@
+package com.dj.journalApp.service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail() {
+        emailService.sendEmail("dummy2@gmail.com",
+                "Testing Java mail sender",
+                "Hi, How are you?");
+    }
+}
