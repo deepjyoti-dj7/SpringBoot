08053cf3990911552bf0b07e4dfc413c4559818e
Deepjyoti <deepjyoti0072@gmail.com>
2025-07-09 00:22:19 +0530

    Added cron Scheduler

---
+++ src/main/java/com/dj/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,40 @@
+package com.dj.journalApp.scheduler;
+
+import com.dj.journalApp.entity.JournalEntry;
+import com.dj.journalApp.entity.User;
+import com.dj.journalApp.repository.UserRepositoryImpl;
+import com.dj.journalApp.service.EmailService;
+import com.dj.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    @Scheduled(cron = "0 0 9 * * SUN")
+    public void fetchUsersAndSendSAMail() {
+        List< User> users = userRepository.getUserForSA();
+        for (User user : users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+        }
+    }
+}
