4e851297c4e78f202f68b2c5a267df6473616d97
Deepjyoti <deepjyoti0072@gmail.com>
2025-07-24 00:32:28 +0530

    Updated DTO class

--- src/main/java/com/dj/journalApp/dto/UserDTO.java
+++ src/main/java/com/dj/journalApp/dto/UserDTO.java
@@ -1,22 +1,21 @@
 package com.dj.journalApp.dto;
 
-//import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-//import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
 
-//    @NotEmpty
-//    @Schema(description = "The user's username")
+    @NotEmpty
+    @Schema(description = "The user's username")
     private String userName;
     private String email;
     private boolean sentimentAnalysis;
-//    @NotEmpty
+    @NotEmpty
     private String password;
 }
